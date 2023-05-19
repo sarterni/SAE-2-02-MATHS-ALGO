783fd9c1733565fc07734d6be0804aa94172583f
sarterni <sarter.nicolas@gmail.com>
2023-05-19 17:10:25 +0200

    modifs

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -7,15 +7,26 @@ public class GrapheListe implements Graphe {
     private List<Noeud> ensNoeuds;
 
     public void ajouterArc(String depart, String destination, double cout){
-
+        this.ensNom.add(depart);
+        
+        List<Arc> tmp = new List<Arc>();
+        tmp.add(new Arc(destination, cout));
+        this.ensNoeuds.add(new Noeud(depart, tmp));
     }
 
     public List<String> listeNoeuds(){
+        return ensNom;
 
     }
 
     public List<Arc> suivants(String n){
-        
+        List<Arc> res = null;
+        for(int i = 0; i < this.ensNom.size(); i++){
+            if(this.ensNom.get(i).equals(this.ensNoeuds.get(i).getNom())){
+                res = this.ensNoeuds.get(i).getAdj();
+            }
+        }
+        return res;
     }
     
 }
