c939d48c8a9c9667c67ee5c5d1ab71521c3e310e
sarterni <sarter.nicolas@gmail.com>
2023-05-19 16:33:09 +0200

    graphelist

---
+++ src/GrapheListe.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class GrapheListe implements Graphe {
+
+    //attributs
+    private List<String> ensNom;
+    private List<Noeud> ensNoeuds;
+
+    public void ajouterArc(String depart, String destination, double cout){
+
+    }
+
+    public List<String> listeNoeuds(){
+
+    }
+
+    public List<Arc> suivants(String n){
+        
+    }
+    
+}
