6cfb9d0a07078172fcc65ef7f13e55e7894da84e
Failkun2 <felicien.kinnig9@etu.univ-lorraine.fr>
2023-05-25 15:59:18 +0200

    a

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dijkstra {
+    //    Algorithme de Dijkstra
+    //    Entrées :
+    //    G un graphe orienté avec une pondération (poids) positive des arcs
+    //    A un sommet (départ) de G
+    //    Début
+    //    Q <- {} // utilisation d’une liste de noeuds à traiter
+    //    Pour chaque sommet v de G faire
+    //      v.distance <- Infini
+    //      v.parent <- Indéfini
+    //      Q <- Q U {v} // ajouter le sommet v à la liste Q
+    //    Fin Pour
+    //    A.distance <- 0
+    //    Tant que Q est un ensemble non vide faire
+    //      u <- un sommet de Q telle que u.distance est minimale
+    //      Q <- Q \ {u} // enlever le sommet u de la liste Q
+    //      Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+    //          D <- u.distance + poids(u,v)
+    //          Si D < v.distance
+    //              Alors v.distance <- D
+    //                  v.parent <- u
+    //          Fin Si
+    //      Fin Pour
+    //    Fin Tant que
+    //    Fin
     
+    // q: convertir en Java
+    // a:
+    //a:
+    public Valeur resoudre(Graphe g, String depart){
+        Valeur res = new Valeur();
+        List<Noeud> Q = new ArrayList<Noeud>();
+        for(Noeud v : g){
+            
+        }
+    }
 }
