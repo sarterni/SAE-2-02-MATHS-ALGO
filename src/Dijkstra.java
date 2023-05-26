2bbc0c5f50b2432a8f10c96d7f12feda4ead4d54
sarterni <sarter.nicolas@gmail.com>
2023-05-26 20:46:55 +0200

    dijstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,6 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
 
-    
-    public class Dijkstra {
+
+public class Dijkstra {
+
+        public Graphe g;
+        public Vertex a;
+
+            //Q <- {} // utilisation d’une liste de noeuds à traiter
+            List<Vertex> q = new ArrayList<Vertex>();
+            //Pour chaque sommet v de G faire
+            for (Vertex v : g.getVertices()) {
+                //v.distance <- Infini
+                v.setDistance(Integer.MAX_VALUE);
+                //v.parent <- Indéfini
+                v.setParent(null);
+                //Q <- Q U {v} // ajouter le sommet v à la liste Q
+                q.add(v);
+            }
+            //A.distance <- 0
+            a.setDistance(0);
+            //Tant que Q est un ensemble non vide faire
+            while (!q.isEmpty()) {
+                //u <- un sommet de Q telle que u.distance est minimale
+                Vertex u = null;
+                int min = Integer.MAX_VALUE;
+                for (Vertex v : q) {
+                    if (v.getDistance() < min) {
+                        min = v.getDistance();
+                        u = v;
+                    }
+                }
+                //Q <- Q \ {u} // enlever le sommet u de la liste Q
+                q.remove(u);
+                //Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+                for (Vertex v : q) {
+                    if (g.getEdge(u, v) != null) {
+                        //D <- u.distance + poids(u,v)
+                        int d = u.getDistance() + g.getEdge(u, v).getWeight();
+                        //Si D < v.distance
+                        if (d < v.getDistance()) {
+                            //Alors v.distance <- D
+                            v.setDistance(d);
+                            //v.parent <- u
+                            v.setParent(u);
+                        }
+                    }
+                }
+            }
+        }
  
 
     }
