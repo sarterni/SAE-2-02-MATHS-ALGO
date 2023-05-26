60143afec5e559dd1076e4f6760a0d56d08ea9f1
sarterni <sarter.nicolas@gmail.com>
2023-05-26 21:36:32 +0200

    modifs

---
+++ src/MainDijktra.java
@@ -0,0 +1,18 @@
+public class MainDijktra {
+    public static void main(String[] args) {
+        //        G <- Graphe construit à partir du fichier texte
+        Graphe g = new Graphe() {
+            // g.loadFromFile("graph1.txt");
+            //        A <- Sommet de G
+            Vertex a = g.getVertex("A");
+            //        Dijkstra(G,A)
+            Dijkstra d = new Dijkstra(g, a);
+            //        Pour chaque sommet v de G faire
+            for (Vertex v : g.getVertices()) {
+                //            Afficher v.label, v.distance, v.parent.label
+                System.out.println(v.getLabel() + " " + v.getDistance() + " " + v.getParent().getLabel());
+            }
+        }
+    }
+}
+
